f3734d866b88fb24babf0f71b67b26742a8b224b
llabaka <lander.labaka@ikasle.aeg.eus>
2024-09-26 20:24:56 +0200

    [ER6-34][ADD] Modal with qr in acolyte lab

--- components/labScreen.tsx
+++ components/labScreen.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Modal, Button, TouchableOpacity } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
-type LabScreenProps = {
-
-}
+type LabScreenProps = {}
 
 const LabScreen: React.FC<LabScreenProps> = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+    }
+
     return (
         <View style={styles.container}>
-        <Text style={styles.title}>Welcome to Kaotika's Laboratory</Text>
-        {/* Agrega más contenido o componentes aquí */}
+            <Text style={styles.title}>Welcome to Kaotika's Laboratory</Text>
+            <TouchableOpacity onPress={toggleModal} style={styles.button}>
+                <Text style={styles.buttonText}>Open QR</Text>
+            </TouchableOpacity>
+            
+            <Modal
+                visible={modalVisible}
+                transparent={true}
+                animationType="fade"
+                onRequestClose={toggleModal}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <QRCode
+                            value="" // El valor del qr sera el que queramos
+                            size={200}
+                        />
+                        <View style={styles.buttonContainer}>
+                            <Button title="Close" onPress={toggleModal} />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
-    };
+};
 
-    const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -25,6 +51,30 @@ const LabScreen: React.FC<LabScreenProps> = () => {
         fontSize: 30,
         fontWeight: 'bold',
         marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#007bff',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semitransparente para el modal
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        marginTop: 10, // Esto mueve el botón más abajo
     },
 });
 
