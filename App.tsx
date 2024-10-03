0fb30a5ef8c07eb3e329a5ea85c429675c983ce7
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-10-04 01:18:30 +0200

    [ER6-53]IP changes

--- App.tsx
+++ App.tsx
@@ -38,7 +38,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-export const socket = io('http://192.168.1.89:3000');
+export const socket = io('http://192.168.1.132:3000');
 
 
 
@@ -238,7 +238,7 @@ function App(): React.JSX.Element {
       // console.log('Token de ID:', idTokenResult);
 
       // Envía el idToken al servidor
-      const fireBaseResponse = await fetch('http://192.168.1.89:3000/verify-token', {
+      const fireBaseResponse = await fetch('http://192.168.1.132:3000/verify-token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
