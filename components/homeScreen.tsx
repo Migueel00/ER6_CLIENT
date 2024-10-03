6af96bdc6044d3278f6efe0a41c250caa04941b8
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-03 12:20:14 +0200

    [ER6-52][UPDATE] Changed HomeScreen background image

--- components/homeScreen.tsx
+++ components/homeScreen.tsx
@@ -7,20 +7,24 @@ type HomeScreenProps = {
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ role }) => {
     return (
-
         <ImageBackground 
-            source={require('../assets/png/LabEntrance.png')} // Cambia esta ruta a la imagen que desees
+            source={require('../assets/png/HomeScreen.png')} // Cambia esta ruta a la imagen que desees
             style={styles.background}
             resizeMode="cover" // Asegúrate de que la imagen cubra todo el área
         >
             <View style={styles.container}>
-                <Text style={styles.roboto}>Welcome to Kaotika, {role}</Text>
+                <Text style={styles.kaotikaFont}>
+                    Welcome to <Text style={styles.kaotika}>KA<Text style={styles.o}>O</Text>TIKA</Text>
+                </Text>
+                <Text style={styles.kaotikaFont2}>
+                    {role}
+                </Text>
             </View>
         </ImageBackground>
     );
-    };
+};
 
-    const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     background: {
         flex: 1,
         justifyContent: 'center',
@@ -28,10 +32,11 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ role }) => {
     },
     container: {
         flex: 1, 
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         width: '100%', 
         height: '100%', 
+        paddingTop: 20
     },
     title: {
         fontSize: 30,
@@ -39,9 +44,22 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ role }) => {
         marginBottom: 20,
         fontFamily: 'Roboto-Thin'
     },
-    roboto: {
+    kaotikaFont: {
         fontFamily: 'KochAltschrift',
-        fontSize: 30
+        fontSize: 40,
+        color: 'white', // Color blanco para el texto principal
+    },
+    kaotikaFont2: {
+        paddingTop: 20,
+        fontFamily: 'KochAltschrift',
+        fontSize: 40,
+        color: 'white', // Color blanco para el texto principal
+    },
+    kaotika: {
+        color: 'white', // Color blanco para "Kati" y "ka"
+    },
+    o: {
+        color: 'orange', // Color naranja para la "o"
     }
 });
 
