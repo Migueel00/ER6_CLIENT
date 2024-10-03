57572c476098aa09cf47dd67c740d440cc0c7d5b
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-10-03 09:59:17 +0200

    [ER6-48][NEW]APPLY SIGN OUT BUTTON CORRECTLY

--- components/settings/settingsScreen.tsx
+++ components/settings/settingsScreen.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import Button from '../button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 type SettingScreenProps = {
-
+    setIsLoggedIn: any;
 }
 
-const SettingsScreen: React.FC<SettingScreenProps> = () => {
+const SettingsScreen: React.FC<SettingScreenProps> = ({setIsLoggedIn}) => {
+    const signOut = async () => {
+        try {
+            await GoogleSignin.signOut();
+        }
+        catch (error){
+
+            console.error(error);
+        }
+    }
+
+    const onPress = () => {
+        setIsLoggedIn(false);
+        AsyncStorage.clear();
+        signOut();
+    }
+
     return (
         <View style={styles.container}>
         <Text style={styles.title}>Your are in settings!</Text>
         {/* Agrega más contenido o componentes aquí */}
+        <Button onPress={onPress} title='Log out'></Button>
         </View>
     );
     };
