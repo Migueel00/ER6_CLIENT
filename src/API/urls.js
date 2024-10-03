d0f4727e3b09d186dbb1546bc3150ad58873c110
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-03 19:53:32 +0200

    [ER6-56][UPDATE] Changed ip's

--- src/API/urls.js
+++ src/API/urls.js
@@ -1,8 +1,8 @@
 
 export const URL = {
-    GET_ALL_PLAYERS: 'http://10.70.0.82:3000/api/players',
-    API_PLAYERS:     'http://10.70.0.82:3000/api/players',
-    UPDATE_PLAYER_BY_EMAIL: 'http://10.70.0.82:3000/api/players/email'
+    GET_ALL_PLAYERS: 'http://192.168.1.150:3000/api/players',
+    API_PLAYERS:     'http://192.168.1.150:3000/api/players',
+    UPDATE_PLAYER_BY_EMAIL: 'http://192.168.1.150:3000/api/players/email'
 
 }
 
