08c6e4b8ae02a5962702cc645887d7e9dd147980
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-11 10:01:35 +0200

    [ER6-73][UPDATE] Profile component working with appcontext as intended

--- components/profileScreen2.tsx
+++ components/profileScreen2.tsx
@@ -1,92 +1,96 @@
 import React from 'react';
 import { View, Text, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 import * as Progress from 'react-native-progress';
+import AppContext from '../helpers/context';
 
-type ProfileScreenProps = {
-    profileAttributesToPrint: any;
-};
-
-const convertAttributesToPercentage = (profileAttributesToPrint: any) => {
+const convertAttributesToPercentage = (profileAttributes: any) => {
     return {
-        intelligence: profileAttributesToPrint.intelligence / 100,
-        dexterity: profileAttributesToPrint.dexterity / 100,
-        insanity: profileAttributesToPrint.insanity / 100,
-        charisma: profileAttributesToPrint.charisma / 100,
-        constitution: profileAttributesToPrint.constitution / 100,
-        strength: profileAttributesToPrint.strength / 100,
+        intelligence: profileAttributes.intelligence / 100,
+        dexterity: profileAttributes.dexterity / 100,
+        insanity: profileAttributes.insanity / 100,
+        charisma: profileAttributes.charisma / 100,
+        constitution: profileAttributes.constitution / 100,
+        strength: profileAttributes.strength / 100,
     };
 };
 
-const ProfileScreen2: React.FC<ProfileScreenProps> = ({ profileAttributesToPrint }) => {
-    const {height, width} = Dimensions.get('window')
-    const attributesForProgressBar = convertAttributesToPercentage(profileAttributesToPrint);
+const ProfileScreen2 = () => {
+    const { height, width } = Dimensions.get('window');
 
     return (
-        <ImageBackground 
-            source={require('../assets/png/profileBackground.png')}
-            style={[styles.background, { width: width, height: height }]}
-        >
-            <View style={[styles.container, { padding: height * 0.04 }]}>
-            <View style={styles.titleContainer}>
-                    <Text style={styles.titleText}>Character Profile</Text>
-                </View>
+        <AppContext.Consumer>
+            {({ profileAttributes }: any) => {
+                const attributesToPrint = convertAttributesToPercentage(profileAttributes);
 
-                <View style={styles.progressContainer}>
-                    <View style={styles.column}>
-                        <Text style={styles.profileText}>Intelligence</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.intelligence} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
+                return (
+                    <ImageBackground 
+                        source={require('../assets/png/profileBackground.png')}
+                        style={[styles.background, { width: width, height: height }]}
+                    >
+                        <View style={[styles.container, { padding: height * 0.04 }]}>
+                            <View style={styles.titleContainer}>
+                                <Text style={styles.titleText}>Character Profile</Text>
+                            </View>
 
-                        <Text style={styles.profileText}>Dexterity</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.dexterity} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
+                            <View style={styles.progressContainer}>
+                                <View style={styles.column}>
+                                    <Text style={styles.profileText}>Intelligence</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.intelligence} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
 
-                        <Text style={styles.profileText}>Insanity</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.insanity} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
-                    </View>
+                                    <Text style={styles.profileText}>Dexterity</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.dexterity} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
 
-                    <View style={styles.column}>
-                        <Text style={styles.profileText}>Charisma</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.charisma} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
+                                    <Text style={styles.profileText}>Insanity</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.insanity} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
+                                </View>
 
-                        <Text style={styles.profileText}>Constitution</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.constitution} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
+                                <View style={styles.column}>
+                                    <Text style={styles.profileText}>Charisma</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.charisma} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
 
-                        <Text style={styles.profileText}>Strength</Text>
-                        <Progress.Bar 
-                            progress={attributesForProgressBar.strength} 
-                            width={width * 0.20} 
-                            color='orange' 
-                        />
-                    </View>
-                </View>
-            </View>
-        </ImageBackground>
+                                    <Text style={styles.profileText}>Constitution</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.constitution} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
+
+                                    <Text style={styles.profileText}>Strength</Text>
+                                    <Progress.Bar 
+                                        progress={attributesToPrint.strength} 
+                                        width={width * 0.20} 
+                                        color='orange' 
+                                    />
+                                </View>
+                            </View>
+                        </View>
+                    </ImageBackground>
+                );
+            }}
+        </AppContext.Consumer>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'space-between', // Cambiado a flex-start para que el contenido comience desde la parte superior
+        justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: 'transparent',
         padding: 30,
@@ -106,16 +110,15 @@ const styles = StyleSheet.create({
         fontFamily: 'KochAltschrift',
         fontSize: 30,
         padding: 5,
-        
     },
-        titleContainer: {
+    titleContainer: {
         borderColor: 'orange', 
         borderWidth: 2,
         borderRadius: 10, 
         padding: 5, 
         marginBottom: 20, 
         backgroundColor: 'rgba(0, 0, 0, 0.5)'
-},
+    },
     progressContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -127,8 +130,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     column: {
-        flex: 1, // Cada columna ocupa el mismo espacio
-        alignItems: 'center', // Centra los elementos dentro de cada columna
+        flex: 1,
+        alignItems: 'center',
     },
 });
 
