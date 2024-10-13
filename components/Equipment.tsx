014ae6f5fa1709fcc37613121e09e40acc28b9e7
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-13 21:55:11 +0200

    [ER6-79][ADD] Add stats with progress bar to stats component

--- components/Equipment.tsx
+++ components/Equipment.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const Equipment = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Equipment Content Here</Text>
+            <Text style={styles.text}>Equipment</Text>
         </View>
     );
 };
