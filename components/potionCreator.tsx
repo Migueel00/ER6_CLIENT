dddb070b84dedae55e966eb4c7214658cf54aaa9
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-14 18:05:04 +0200

    [ER6-74][UPDATE] Receiving ingredients data from kaotika api

--- components/potionCreator.tsx
+++ components/potionCreator.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, SafeAreaView, ImageBackground, StyleSheet } from 'react-native';
 
 // Obtén la imagen de fondo de los assets locales
 const backgroundImageURL = require('../assets/png/settingsBackground1.png');
 
+// Construir la URL con el email del alumno
+const kaotikaIngredients = `https://kaotika-server.fly.dev/players/email/`;
+
 const { width, height } = Dimensions.get('window');
 
 const PotionCreator = () => {
+
+    const [ingredientsData, setIngredientsData] = useState(null);
+
+    useEffect(() => {
+        const getIngredients = async () => {
+            try {
+                const response = await fetch ('https://kaotika-server.fly.dev/ingredients')
+
+                if (!response.ok){
+                    throw new Error('Error en la respuesta de la API');
+                }
+
+                const jsonData = await response.json();
+
+                setIngredientsData(jsonData);
+                
+            } catch (error){
+                console.log(error);
+            }
+        }
+
+        getIngredients();
+    }, [])
+    
     return (
         <SafeAreaView style={{flex: 1}}>
             <ImageBackground
