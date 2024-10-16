f2a6a9f69206b4560056eafc0aa38e68cac909dd
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-16 09:23:40 +0200

    [ER6-76][UPDATE] Antidote and Poison created

--- components/potions/execute.tsx
+++ components/potions/execute.tsx
@@ -18,14 +18,14 @@ const executePotionCreation = async() => {
         const ingredients = Ingredients.load(fakeIngredients).ingredients;
         const curses = Curses.load(fakeCurses).curses;
 
-        console.log("CURSES");
-        console.log(curses);
+        //console.log("CURSES");
+        //console.log(curses);
         
         //console.log("INGREDIENTS: " + ingredients);
         
         //console.log("FIRST INGREDIENT: " +  ingredients[0].name);
         
-        const ingredientsArray = [ingredients[50], ingredients[50], ingredients[50]]
+        const ingredientsArray = [ingredients[14], ingredients[20]]
 
         const cauldron = new Cauldron(ingredients, curses);
 
