d23b30a45b49234c7a9b30f3dd83a6146f1fd682
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-18 09:49:53 +0200

    [FIX-EYE][UPDATE] Eye button style

--- components/ScannerScreen.tsx
+++ components/ScannerScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Dimensions, Image, ImageBackground, Modal, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Dimensions, Modal, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import styled from 'styled-components/native';
 import CameraScreen from './cameraScreen';
 import AppContext from '../helpers/context';
 
+const { height, width } = Dimensions.get('window');
+
 const ScannerScreen = () => {
-    const {height, width} = Dimensions.get('window')
     // Estado para manejar el modal de la cámara
     const [isCameraModalVisible, setCameraModalVisible] = useState(false);
 
@@ -21,81 +21,64 @@ const ScannerScreen = () => {
     };
 
     const socket = useContext(AppContext)?.socket;
-    
-    useEffect(() => {
 
+    useEffect(() => {
         // Cambiar isInsideLab cuando se recibe OK! desde el servidor
         socket.on('ScanSuccess', (message: string) => {
             console.log("Mensaje del servidor:", message);
-
             setCameraModalVisible(false);
         });
 
         return () => {
-            socket.off('ScanSuccess')
-        }
+            socket.off('ScanSuccess');
+        };
     }, []);
 
     return (
-        <ImageBackground
-                source={require('../assets/png/cameraScreenEye.png')}
-                style={[styles.background, { width: width, height: height }]}
-            >
-                <View style={styles.container}> 
-                    <Text style={styles.kaotikaFont}>Touch the eye to use</Text>
-                    <Text style={styles.kaotikaFont}>Morghul's Sight</Text>
+        <Background source={require('../assets/png/cameraScreenEye.png')} style={{ width, height }}>
+            <Container>
+                <KaotikaText>Touch the eye to use</KaotikaText>
+                <KaotikaText>Morghul's Sight</KaotikaText>
 
-                    <Modal
-                        visible={isCameraModalVisible}
-                        animationType="fade"
-                        onRequestClose={closeCameraModal}
-                    >
-                        <CameraScreen onClose={closeCameraModal} />
-                    </Modal>
-                </View>
+                <Modal
+                    visible={isCameraModalVisible}
+                    animationType="fade"
+                    onRequestClose={closeCameraModal}
+                >
+                    <CameraScreen onClose={closeCameraModal} />
+                </Modal>
+            </Container>
 
-                <TouchableOpacity onPress={openCameraModal} style={styles.eyeButton}>
-
-                </TouchableOpacity>
-            </ImageBackground>
+            <EyeButton onPress={openCameraModal} />
+        </Background>
     );
 };
 
-const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-    },
-    eyeButton: {
-        height: 135,
-        width: 135,
-        opacity: 0, // Aplica la opacidad al contenedor del botón
-        backgroundColor: 'purple',
-        borderRadius: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',  // Para centrarlo en la pantalla
-        top: '42.5%',  // Centrado verticalmente
-        left: '45%',  // Centrado horizontalmente
-        transform: [{ translateX: -50 }, { translateY: -50 }],  // Ajuste para centrar el botón
-    },
-    title: {
-        fontSize: 50,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    kaotikaFont: {
-        paddingTop: 20,
-        fontFamily: 'KochAltschrift',
-        fontSize: 40,
-        color: 'white', 
-    },
-});
+const Background = styled.ImageBackground`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Container = styled.View`
+    flex: 1;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const EyeButton = styled.TouchableOpacity`
+    height: ${height * 0.4}px;
+    width: ${width * 0.9}px;
+    background-color: purple;
+    border-radius: ${width * 0.60}px;
+    bottom: ${width * 0.55}px;
+`;
+
+const KaotikaText = styled.Text`
+    padding-top: 20px;
+    font-family: 'KochAltschrift';
+    font-size: 40px;
+    color: white;
+`;
 
 export default ScannerScreen;
