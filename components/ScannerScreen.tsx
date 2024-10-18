36848e784d5b31ecebda894dd2043e6450f87f65
llabaka <lander.labaka@ikasle.aeg.eus>
2024-10-18 10:04:34 +0200

    [FIX-EYE][UPDATE] Styles in eye and texts

--- components/ScannerScreen.tsx
+++ components/ScannerScreen.tsx
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Dimensions, Modal, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
-import styled from 'styled-components/native';
+import { Dimensions, Image, ImageBackground, Modal, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import CameraScreen from './cameraScreen';
 import AppContext from '../helpers/context';
 
-const { height, width } = Dimensions.get('window');
-
+const {height, width} = Dimensions.get('window')
 const ScannerScreen = () => {
+
     // Estado para manejar el modal de la cámara
     const [isCameraModalVisible, setCameraModalVisible] = useState(false);
 
@@ -21,64 +22,73 @@ const ScannerScreen = () => {
     };
 
     const socket = useContext(AppContext)?.socket;
-
+    
     useEffect(() => {
+
         // Cambiar isInsideLab cuando se recibe OK! desde el servidor
         socket.on('ScanSuccess', (message: string) => {
             console.log("Mensaje del servidor:", message);
+
             setCameraModalVisible(false);
         });
 
         return () => {
-            socket.off('ScanSuccess');
-        };
+            socket.off('ScanSuccess')
+        }
     }, []);
 
     return (
-        <Background source={require('../assets/png/cameraScreenEye.png')} style={{ width, height }}>
-            <Container>
-                <KaotikaText>Touch the eye to use</KaotikaText>
-                <KaotikaText>Morghul's Sight</KaotikaText>
+        <ImageBackground
+                source={require('../assets/png/cameraScreenEye.png')}
+                style={[styles.background, { width: width, height: height }]}
+            >
+                <View style={styles.container}> 
+                    <Text style={styles.kaotikaFont}>Touch the eye to use</Text>
+                    <Text style={styles.kaotikaFont}>Morghul's Sight</Text>
 
-                <Modal
-                    visible={isCameraModalVisible}
-                    animationType="fade"
-                    onRequestClose={closeCameraModal}
-                >
-                    <CameraScreen onClose={closeCameraModal} />
-                </Modal>
-            </Container>
+                    <Modal
+                        visible={isCameraModalVisible}
+                        animationType="fade"
+                        onRequestClose={closeCameraModal}
+                    >
+                        <CameraScreen onClose={closeCameraModal} />
+                    </Modal>
+                </View>
 
-            <EyeButton onPress={openCameraModal} />
-        </Background>
+                <TouchableOpacity onPress={openCameraModal} style={styles.eyeButton}>
+
+                </TouchableOpacity>
+            </ImageBackground>
     );
 };
 
-const Background = styled.ImageBackground`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-`;
-
-const Container = styled.View`
-    flex: 1;
-    justify-content: flex-start;
-    align-items: center;
-`;
-
-const EyeButton = styled.TouchableOpacity`
-    height: ${height * 0.4}px;
-    width: ${width * 0.9}px;
-    background-color: purple;
-    border-radius: ${width * 0.60}px;
-    bottom: ${width * 0.55}px;
-`;
-
-const KaotikaText = styled.Text`
-    padding-top: 20px;
-    font-family: 'KochAltschrift';
-    font-size: 40px;
-    color: white;
-`;
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    eyeButton: {
+        height: height *0.4,
+        width: width *0.8,
+        opacity: 0, // Aplica la opacidad al contenedor del botón
+        backgroundColor: 'purple',
+        borderRadius: height * 0.2,
+        top: height * - 0.2,  // Centrado verticalmente
+        left: width * 0.12,  // Centrado horizontalmente
+        transform: [{ translateX: -50 }, { translateY: -50 }],  // Ajuste para centrar el botón
+    },
+    kaotikaFont: {
+        paddingTop: height * 0.02,
+        fontFamily: 'KochAltschrift',
+        fontSize: width * 0.12,
+        color: 'white', 
+    },
+});
 
 export default ScannerScreen;
