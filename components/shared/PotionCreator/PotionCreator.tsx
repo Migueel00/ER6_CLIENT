497777b14f3172f383207c3cb166eb9dab4076bc
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-10-22 01:03:47 +0200

    [ER6-119][NEW]REFACTOR POTION CREATOR COMPONENT

--- components/shared/PotionCreator/PotionCreator.tsx
+++ components/shared/PotionCreator/PotionCreator.tsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Animated, Dimensions, ImageBackground, Modal, StatusBar, StyleSheet, ToastAndroid, TouchableWithoutFeedback, Vibration, FlatList } from 'react-native';
+import { Animated, Dimensions, ImageBackground, Modal, StatusBar, StyleSheet, ToastAndroid, Vibration} from 'react-native';
 import styled from 'styled-components/native';
 import AppContext from '../../../helpers/context';
 import Cauldron from '../../potions/cauldron';
 import Ingredient from '../../potions/ingredient';
 import Potion from '../../potions/potion';
 import FilterModal from './FilterModal';
+import PotionModal from './components/PotionModal';
+import FlatListIngredients from './components/FlatListIngredients';
 
 const backgroundImageURL = require('../../../assets/png/settingsBackground1.png');
 const defaultPotionImage = require('../../../assets/png/ingredients.jpeg');
@@ -28,25 +30,10 @@ const CONSTANTS = {
     BUTTON_RIGHT: 0.05
 };
 
-// Función para formatear los efectos
-const formatEffects = (effects: string[]): string => {
-    return effects
-        .map(effect =>
-            effect
-                .split('_') // Divide los guiones bajos
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Primera letra de cada palabra mayúscula
-                .join(' ') // Une las palabras con espacios
-        )
-        .join(', '); // Une los diferentes efectos con comas
-};
-
 const PotionCreator = () => {
-    const flatListRef = useRef<Animated.FlatList>(null); 
-
     const [selectedIngredient, setSelectedIngredient] = useState<{ name: string, effects: string }>({ name: '', effects: '' });
     const [selectedIngredientArray, setSelectedIngredientArray] = useState<Ingredient[]>([]);
     const context = useContext(AppContext);
-    const userRole = context?.player?.role;
     const [potionFactory, setPotionFactory] = useState<Cauldron | null>();
     const [curses, setCurses] = useState(require('../../../fakedata/fake-curses.json'));
 
@@ -57,7 +44,6 @@ const PotionCreator = () => {
     const [showBackButton, setShowBackButton] = useState(false);
     const [showCreatePotionButton, setShowCreatePotionButton] = useState(true);
     const [filterBooleans, setFilterBooleans] = useState<boolean[]>([]);
-    const [flatlistIndex, setFlatlistIndex] = useState(0);
 
     const toggleModal = () => {
         console.log("ENTRA A TOGGLE MODAL");
@@ -65,13 +51,8 @@ const PotionCreator = () => {
         setPotionModalVisible(!potionModalVisible);
         setSelectedIngredientArray([]); 
     };
+
     const [filterModalVisible, setFilterModalVisible] = useState<boolean>(false);
-
-    const scrollX = useRef(new Animated.Value(0)).current;
-
-    useEffect(() => {
-        
-    }, [ingredients]); 
 
     useEffect(() => {
         if (!potionFactory) {
@@ -79,19 +60,8 @@ const PotionCreator = () => {
         }
     }, []);
 
-    useEffect(() => {
-        console.log("HA ENTRADO A HACER EL SCROLL AL INICIO");
-        
-        // Desplazar FlatList al índice 0 cuando cambian los ingredientes
-        if (flatListRef.current) {
-            console.log("CURRENT EXISTE");
-            
-            flatListRef.current.scrollToIndex({ index: 0, animated: true });
-        }
-    }, [ingredientsCopy, ingredients, setIngredientCopy, setIngredients]);
 
     const handlePressFilter = () => {
-        //console.log(filterBooleans);
         setFilterModalVisible(true);
     }
 
@@ -100,51 +70,8 @@ const PotionCreator = () => {
         if (selectedIngredientArray.length < 4) {
             ToastAndroid.show(ingredient.name + " added", ToastAndroid.SHORT);
             Vibration.vibrate(100); 
-            <Animated.FlatList
-                initialNumToRender={ingredients.length}
-                maxToRenderPerBatch={ingredients.length}
-                updateCellsBatchingPeriod={ingredients.length}
-                ref={flatListRef} 
-                snapToInterval={CONSTANTS.ITEM_SIZE}
-                decelerationRate={0}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ alignItems: 'center' }}
-                scrollEventThrottle={16}
-                horizontal
-                data={ingredientsCopy}
-                keyExtractor={(item) => item._id ? item._id.toString() : item.key}
-                onScroll={Animated.event(
-                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                    { useNativeDriver: true }
-                )}
-                renderItem={({ item, index }) => {
-                    if (!item.name) return <DummyContainer />;
-                    //Por ahora se usara una imagen local
-                    const imageSource = defaultPotionImage;
-                    const inputRange = [
-                        (index - 2) * CONSTANTS.ITEM_SIZE,
-                        (index - 1) * CONSTANTS.ITEM_SIZE,
-                        index * CONSTANTS.ITEM_SIZE
-                    ];
-                    const translateY = scrollX.interpolate({
-                        inputRange,
-                        outputRange: [-20, -50, -20]
-                    });
-                    
-                    return (
-                        <TouchableWithoutFeedback onLongPress={() => handleLongPress(item)}>
-                            <IngredientContainer>
-                                <IngredientItem as={Animated.View} style={{ transform: [{ translateY }] }}>
-                                    <IngredientName>{item.name}</IngredientName>
-                                    <IngredientImage source={imageSource} />
-
-                                    <IngredientEffects>{formatEffects(item.effects)}</IngredientEffects>
-                                </IngredientItem>
-                            </IngredientContainer>
-                        </TouchableWithoutFeedback>
-                    );
-                }}
-            />
+            // Preguntar si esta linea es realmente necesaria??
+            // <FlatListIngredients ingredients={ingredients} handleLongPress={handleLongPress}/>
             console.log("Ingrediente seleccionado");
             setSelectedIngredientArray(prev => [...prev, ingredient]);
         }
@@ -168,56 +95,11 @@ const PotionCreator = () => {
         <Container>
             <StatusBar />
             <ImageBackground source={backgroundImageURL} style={styles.backgroundImage}>
-                <FlatListView>
-                    <Animated.FlatList
-                        initialNumToRender={ingredients.length}
-                        maxToRenderPerBatch={ingredients.length}
-                        updateCellsBatchingPeriod={ingredients.length}
-                        ref={flatListRef} 
-                        snapToInterval={CONSTANTS.ITEM_SIZE}
-                        decelerationRate={0}
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={{ alignItems: 'center' }}
-                        scrollEventThrottle={16}
-                        horizontal
-                        data={ingredientsCopy}
-                        keyExtractor={(item) => item._id ? item._id.toString() : item.key}
-                        onScroll={Animated.event(
-                            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                            { useNativeDriver: true }
-                        )}
-                        renderItem={({ item, index }) => {
-                            if (!item.name) return <DummyContainer />;
-                            //Por ahora se usara una imagen local
-                            const imageSource = defaultPotionImage;
-                            const inputRange = [
-                                (index - 2) * CONSTANTS.ITEM_SIZE,
-                                (index - 1) * CONSTANTS.ITEM_SIZE,
-                                index * CONSTANTS.ITEM_SIZE
-                            ];             
-                            const translateY = scrollX.interpolate({
-                                inputRange,
-                                outputRange: [-20, -50, -20]
-                            });
-                            return (
-                                <TouchableWithoutFeedback onLongPress={() => handleLongPress(item)}>
-                                    <IngredientContainer>
-
-                                        <IngredientItem as={Animated.View} style={{ transform: [{ translateY }] }}>
-                                            <IngredientName>{item.name}</IngredientName>
-                                            <IngredientImage source={imageSource} />
-
-                                            <IngredientEffects>{formatEffects(item.effects)}</IngredientEffects>
-                                        </IngredientItem>
-                                    </IngredientContainer>
-                                </TouchableWithoutFeedback>
-                            );
-                        }}
-                    />
-                    <FilterButton onPress={handlePressFilter}>
-                        <FilterIcon source={filterIconImage} />
-                    </FilterButton>
-                </FlatListView>
+                {/* Flalist de los ingredientes */}
+                <FlatListIngredients ingredients={ingredientsCopy} handleLongPress={handleLongPress}/>
+                <FilterButton onPress={handlePressFilter}>
+                    <FilterIcon source={filterIconImage} />
+                </FilterButton>
                 {selectedIngredient.name && (  //Si existe el nombre de la pocion se imprimira el nombre y el efecto
                     <IngredientInfoContainer>
                         <IngredientName numberOfLines={2}>{selectedIngredient.name}</IngredientName>
@@ -290,35 +172,13 @@ const PotionCreator = () => {
                         <BackIcon source={goBackImage} />
                     </IngredientBackButton>
                 )}
-
-                <Modal
+                
+                {/* Potion created Modal */}
+                <PotionModal
                     visible={potionModalVisible}
-                    transparent={true}
-                    animationType="fade"
-                    onRequestClose={toggleModal}
-                >
-                    <ModalContainer>
-                        {/* Imagen de fondo */}
-                        <PotionImageBackground source={require('../../../assets/png/darkModal.png')}>
-                            <PotionCreatedMessage>
-                                Potion Created
-                            </PotionCreatedMessage> 
-
-                            {/* Imagen centrada sobre la imagen principal */} 
-                            <CenteredPotionImage source={require('../../../assets/png/createdPotion.png')}/>
-
-
-
-                            <PotionMessage>
-                                {createdPotion?.name}
-                            </PotionMessage>
-                        </PotionImageBackground>
-
-                        <CloseButton onPress={toggleModal}>
-                            <CloseButtonText>ADD TO INVENTORY</CloseButtonText>
-                        </CloseButton>
-                    </ModalContainer>
-                </Modal>
+                    onClose={toggleModal}
+                    createdPotion={createdPotion}
+                />
                 <Modal
                     visible={filterModalVisible}
                     transparent={true}
@@ -335,8 +195,6 @@ const PotionCreator = () => {
                         setIngredientsCopy={setIngredientCopy}
                     />
                 </Modal>
-
-
             </ImageBackground>
         </Container>
     );
@@ -362,10 +220,6 @@ const CreatePotionButton = styled.TouchableOpacity`
     bottom: ${height * 0.035}px;
 `;
 
-const FlatListView = styled.View`
-    width: 100%;
-    height: ${height * 0.8}px;
-`;
 
 const PotionCreationText = styled.Text`
     position: absolute;
@@ -415,29 +269,6 @@ const GridItem = styled.View`
     margin-bottom: ${height * 0.004}px;
 `;
 
-const IngredientContainer = styled.View`
-    width: ${CONSTANTS.ITEM_SIZE}px;
-    margin-top: ${height * - 0.15}px;
-`;
-
-const IngredientItem = styled.View`
-    margin-horizontal: ${CONSTANTS.SPACING}px;
-    padding: ${CONSTANTS.SPACING}px;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.8);
-    border-width: 1.5px;
-    border-color: #C19A6B;
-    border-radius: ${width * 0.02}px;
-`;
-
-const IngredientImage = styled.Image`
-    width: ${CONSTANTS.ITEM_SIZE * 0.60}px;
-    height: ${CONSTANTS.ITEM_SIZE * 0.60}px;
-    resize-mode: cover;
-    border-width: 1.5px;
-    border-color: #FFF;
-    border-radius: ${width * 0.02}px;
-`;
 
 const IngredientListImage = styled.Image`
     width: ${width * 0.12}px;
@@ -469,9 +300,7 @@ const IngredientEffects = styled.Text`
     margin-top: ${height * 0.005}px;
 `;
 
-const DummyContainer = styled.View`
-    width: ${CONSTANTS.SPACER_ITEM_SIZE}px;
-`;
+
 
 const styles = StyleSheet.create({
     backgroundImage: {
@@ -491,65 +320,6 @@ const styles = StyleSheet.create({
     },
 });
 
-const ModalContainer = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.8);
-    width: 100%;
-    height: 100%;
-
-`;
-
-const PotionImageBackground = styled.ImageBackground` 
-    width: 100%;
-    height: 80%;
-    align-items: center;
-    position: relative;
-    margin-top: ${height*0.05}px;  
-    top: ${height * 0.03}px;
-`;
-
-const PotionMessage = styled.Text`
-    position: relative;
-    top: ${width * 0.03}px; /* Alinea el texto en la parte superior de la imagen */
-    color: #ffffff;
-    font-size: ${width * 0.09}px;
-    font-family: 'KochAltschrift';
-    text-align: center;
-`;
-
-const PotionCreatedMessage = styled.Text`
-    position: relative;
-    color: #ffffff;
-    font-size: ${width * 0.13}px;
-    font-family: 'KochAltschrift';
-    text-align: center;
-    top: ${height*0.02}px;
-`;
-
-const CenteredPotionImage = styled.Image`
-    width: ${width * 0.75}px;
-    height: ${width * 0.75}px;
-    position: relative;
-    resize-mode: contain;
-    margin-top: ${height * 0.04}px;
-`;
-
-const CloseButton = styled.TouchableOpacity`
-    padding:${height * 0.01}px;
-    background-color: #800000;
-    border-radius: ${width * 0.01}px;
-    align-items: center;
-    bottom: ${height * 0.1}px;
-`;
-
-const CloseButtonText = styled.Text`
-    color: #ffffff;
-    font-size: 20px;
-    font-family: 'KochAltschrift';
-`;
-
 const FilterButton = styled.TouchableOpacity`
     position: absolute;
     top: ${CONSTANTS.BUTTON_SPACING * CONSTANTS.HEIGHT}px;
@@ -565,10 +335,6 @@ const FilterButton = styled.TouchableOpacity`
     border-color:  #C19A6B;
 `;
 
-const FilterButttonText = styled.Text`
-    color: black;
-    font-size: ${CONSTANTS.WIDTH * 0.05}px;
-    font-family: 'KochAltschrift';
-`
+
 
 export default PotionCreator;
---
+++ components/shared/PotionCreator/components/PotionModal.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { Dimensions, Modal, Text} from "react-native";
+import styled from "styled-components/native";
+import Potion from "../../../potions/potion";
+
+interface PotionModal {
+    visible: boolean;
+    createdPotion?: Potion | null;
+    onClose: () => void;
+}
+
+const { width, height } = Dimensions.get('window');
+const backgroundImage = require('../../../../assets/png/darkModal.png');
+const createdPotionImage = require('../../../../assets/png/createdPotion.png');
+
+const PotionModal : React.FC<PotionModal> = ({visible, onClose, createdPotion}) => {
+
+    return (
+        <Modal 
+            visible = {visible}
+            transparent = {true}
+            animationType = "fade"
+            onRequestClose = {onClose}>
+            <ModalContainer>
+                {/* Imagen de fondo */}
+                <PotionImageBackground source={backgroundImage}>
+                    <PotionCreatedMessage>
+                        Potion Created
+                    </PotionCreatedMessage>
+
+                    {/* Imagen centrada sobre la imagen principa */}
+                    <CenteredPotionImage source={createdPotionImage}/>
+
+                    <PotionMessage>
+                        {createdPotion?.name}
+                    </PotionMessage>
+
+                    <CloseButton onPress={onClose}>
+                        <CloseButtonText>ADD TO INVETORY</CloseButtonText>
+                    </CloseButton>
+                </PotionImageBackground>
+            </ModalContainer>
+
+        </Modal>
+    );
+}
+
+const ModalContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.8);
+    width: 100%;
+    height: 100%;
+`;
+
+const PotionImageBackground = styled.ImageBackground` 
+    width: 100%;
+    height: 80%;
+    align-items: center;
+    position: relative;
+    margin-top: ${height*0.05}px;  
+    top: ${height * 0.03}px;
+`;
+
+const PotionCreatedMessage = styled.Text`
+    position: relative;
+    color: #ffffff;
+    font-size: ${width * 0.13}px;
+    font-family: 'KochAltschrift';
+    text-align: center;
+    top: ${height*0.02}px;
+`;
+
+const CenteredPotionImage = styled.Image`
+    width: ${width * 0.75}px;
+    height: ${width * 0.75}px;
+    position: relative;
+    resize-mode: contain;
+    margin-top: ${height * 0.04}px;
+`;
+
+const PotionMessage = styled.Text`
+    position: relative;
+    top: ${width * 0.03}px; /* Alinea el texto en la parte superior de la imagen */
+    color: #ffffff;
+    font-size: ${width * 0.09}px;
+    font-family: 'KochAltschrift';
+    text-align: center;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+    padding:${height * 0.01}px;
+    background-color: #800000;
+    border-radius: ${width * 0.01}px;
+    align-items: center;
+    bottom: ${height * 0.1}px;
+`;
+
+const CloseButtonText = styled.Text`
+    color: #ffffff;
+    font-size: 20px;
+    font-family: 'KochAltschrift';
+`;
+
+export default PotionModal;
