c4e543eb099665ea016dd47a39d4bb5b90b63035
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-24 09:57:54 +0200

    [ER6-106][UPDATE] Changed API Routes to render

--- src/API/urls.js
+++ src/API/urls.js
@@ -1,8 +1,8 @@
 
 export const URL = {
-    GET_ALL_PLAYERS: 'http://10.80.128.213:3000/api/players',
-    API_PLAYERS:     'http://10.80.128.213:3000/api/players',
-    UPDATE_PLAYER_BY_EMAIL: 'http://10.80.128.213:3000/api/players/email'
+    GET_ALL_PLAYERS: 'https://er6-staging-server.onrender.com/api/players',
+    API_PLAYERS:     'https://er6-staging-server.onrender.com/api/players',
+    UPDATE_PLAYER_BY_EMAIL: 'https://er6-staging-server.onrender.com/api/players/email'
 
 }
 
