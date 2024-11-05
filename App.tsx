fca17b38547401f585210ec467d01a7a03451664
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-05 13:21:50 +0100

    [ER6-141][ADD] Socket emit of location and playerID

--- App.tsx
+++ App.tsx
@@ -188,7 +188,8 @@ function App(): React.JSX.Element {
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
 
-    const email = 'oskar.calvo@aeg.eus';
+    // const email = 'oskar.calvo@aeg.eus';
+    const email = userInfo.data?.user.email;
     const googleIdToken = userInfo.data?.idToken;
     
     // Create a Google credential with the token
