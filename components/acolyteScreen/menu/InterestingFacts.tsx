1f2354b6e81220a28b4fabfc00ba71391888ba44
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-11-21 09:49:39 +0100

    [ER6-200][ADD] Interesting facts array

---
+++ components/acolyteScreen/menu/InterestingFacts.tsx
@@ -0,0 +1,39 @@
+const funFacts =  
+[   
+    {
+        image: "",
+        title: "Mortimer el Mentor",
+        description: "Archmaster, guru and mentor. Experienced alchemist. Welcomes new acolytes and marks their paths. Staunch enemy of Villain Zárate.",
+    },
+    {
+        image: "",
+        title: "Zarate the Villain",
+        description: "Is a despicable being that threatens with the index finger of its right hand. The terror it generates in its victims is such that they run away in fear of failing more subjects than they already have.",
+    },
+    {
+        image: "",
+        title: "Istvan",
+        description: "Loneliness drove this old pimp insane in the Swamp. Now he only enjoys the company of his own victims. Their nauseating stench makes his proximity unbearable.",
+    },
+    {
+        image: "",
+        title: "Obituary",
+        description: "It's an ancient tomb that houses funeral rites. Legends say that its interior houses 4 mausoleums, which hide the most precious secrets of The Arcane Forge."
+    },
+    {
+        image: "",
+        title: "The Infected Swamp",
+        description: "Inhospitable and humid wasteland. Territory of the Brotherhood of the Singers of Ciénaga. Easy to enter, but practically impossible to leave."
+    },
+    {
+        image: "",
+        title: "Ignatius Gartz",
+        description: "A paladin who fell into decline. Wounded in battle at the end of the Second Era, and currently a Justiciar and Redeemer. He is Zachariah's main enemy."
+    },
+    {
+        image: "",
+        title: "Zachariah",
+        description: "The most infamous of villains. Bastard brother of Ignatius. His destiny as an archmage was cut short in the Mausoleum of Horrors, where he was abducted by the living image of Evil. Terrifying, cruel and merciless."
+    },
+
+]
