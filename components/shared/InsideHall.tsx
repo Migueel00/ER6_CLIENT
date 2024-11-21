e3c561dd23bbe9c9fab0d8ae589eb2dc0aa4335e
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-21 13:35:13 +0100

    [DEVELOP][FIX] Acolyte quantity insidehall

--- components/shared/InsideHall.tsx
+++ components/shared/InsideHall.tsx
@@ -62,11 +62,11 @@ const InsideHall = () => {
         const acolytesInside = insidePlayers.filter(player => player.role === 'ACOLYTE');
         const isMortimerInside = insidePlayers.some(player => player.role === 'MORTIMER');
     
-        if (acolytesInside.length === 1 && !isMortimerInside && retrievedArtifacts.length < 1) {
+        if (acolytesInside.length === 3 && !isMortimerInside && retrievedArtifacts.length < 1) {
             console.log("HALL IS FULL");
             setCallMortimerButton(true);
             setShowArtifacts(false);
-        } else if (acolytesInside.length === 1 && isMortimerInside && retrievedArtifacts.length === 1){
+        } else if (acolytesInside.length === 3 && isMortimerInside && retrievedArtifacts.length === 1){
             setCallMortimerButton(false);
             setShowArtifacts(true);
         }
