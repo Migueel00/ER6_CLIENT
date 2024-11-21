aafee03a632fbd6533d8a77e28d9bb58c33ab4b0
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-11-22 00:06:25 +0100

    [ER6-232][NEW]ADD VALIDATED STATE TO MONGO DB

--- src/API/urls.js
+++ src/API/urls.js
@@ -10,28 +10,28 @@
 //     UPDATE_ARTIFACT: 'http://192.168.127.37:3000/api/artifacts'
 // }
 
-export const URL = {
-    GET_ALL_PLAYERS: 'https://er6-staging-server.onrender.com/api/players',
-    API_PLAYERS:     'https://er6-staging-server.onrender.com/api/players',
-    UPDATE_PLAYER_BY_EMAIL: 'https://er6-staging-server.onrender.com/api/players/email',
-    VERIFY_TOKEN:  'https://er6-staging-server.onrender.com/verify-token',
-    SOCKET: 'https://er6-staging-server.onrender.com',
-    NOTIFICATION: 'https://er6-staging-server.onrender.com/send-notification',
-    GET_ARTIFACTS: 'https://er6-staging-server.onrender.com/api/artifacts',
-    UPDATE_ARTIFACT: 'https://er6-staging-server.onrender.com/api/artifacts'
-}
+// export const URL = {
+//     GET_ALL_PLAYERS: 'https://er6-staging-server.onrender.com/api/players',
+//     API_PLAYERS:     'https://er6-staging-server.onrender.com/api/players',
+//     UPDATE_PLAYER_BY_EMAIL: 'https://er6-staging-server.onrender.com/api/players/email',
+//     VERIFY_TOKEN:  'https://er6-staging-server.onrender.com/verify-token',
+//     SOCKET: 'https://er6-staging-server.onrender.com',
+//     NOTIFICATION: 'https://er6-staging-server.onrender.com/send-notification',
+//     GET_ARTIFACTS: 'https://er6-staging-server.onrender.com/api/artifacts',
+//     UPDATE_ARTIFACT: 'https://er6-staging-server.onrender.com/api/artifacts'
+// }
 
 // Miguel IP CASA
-// export const URL = {
-//     GET_ALL_PLAYERS: 'http://192.168.1.136:3000/api/players',
-//     API_PLAYERS:     'http://192.168.1.136:3000/api/players',
-//     UPDATE_PLAYER_BY_EMAIL: 'http://192.168.1.136:3000/api/players/email',
-//     VERIFY_TOKEN:  'http://192.168.1.136:3000/verify-token',
-//     SOCKET: 'http://192.168.1.136:3000',
-//     NOTIFICATION: 'http://192.168.1.136:3000/send-notification',
-//     GET_ARTIFACTS: 'http://192.168.1.136:3000/api/artifacts',
-//     UPDATE_ARTIFACT: 'http://192.168.1.136:3000/api/artifacts'
-// }
+export const URL = {
+    GET_ALL_PLAYERS: 'http://192.168.1.136:3000/api/players',
+    API_PLAYERS:     'http://192.168.1.136:3000/api/players',
+    UPDATE_PLAYER_BY_EMAIL: 'http://192.168.1.136:3000/api/players/email',
+    VERIFY_TOKEN:  'http://192.168.1.136:3000/verify-token',
+    SOCKET: 'http://192.168.1.136:3000',
+    NOTIFICATION: 'http://192.168.1.136:3000/send-notification',
+    GET_ARTIFACTS: 'http://192.168.1.136:3000/api/artifacts',
+    UPDATE_ARTIFACT: 'http://192.168.1.136:3000/api/artifacts'
+}
 
 
 // Miguel IP Clase
