3faf730c7406adbd1a38dcd7edd24319321159ca
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-24 11:27:02 +0100

    [ER6-339][ADD] Menu Inn and Inn screen

---
+++ components/shared/InnScreen.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import { useContext } from "react";
+import AppContext from "../../helpers/context";
+
+const { width, height } = Dimensions.get('window');
+
+
+const CustomBackground = styled.ImageBackground`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+const background = require('../../assets/backgrounds/obituaryBackground.png');
+
+const InnScreen = () => {
+
+    const appContext = useContext(AppContext);
+    const player = appContext?.player;
+
+    return (
+
+        <CustomBackground source={background}>
+        </CustomBackground>
+
+    )
+}
+
+export default InnScreen;
