5a4a9d892ca25837d56e01a3c14c82ecf3486363
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-31 00:04:00 +0100

    [ER6-341][ADD] Isbetrayer attribute to new player

--- src/API/get&post.js
+++ src/API/get&post.js
@@ -153,7 +153,7 @@ export const updateNewAtributtes = async (responseJSON, playerData) => {
     
     const newPlayerData = playerData;
 
-    const newAtributtes = ['isInsideLab', 'isInsideTower', 'fcmToken', 'location', 'isInsideHall'];
+    const newAtributtes = ['isInsideLab', 'isInsideTower', 'fcmToken', 'location', 'isInsideHall', 'isBetrayer'];
 
     newAtributtes.forEach(attr => {
         if (attr in responseJSON.data) {
