45ecb445b7a7d687a791f1277f09a5c657704b6f
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-31 00:25:27 +0100

    [ER6-342][ADD] Modal when entering to inn

--- components/shared/InnScreen.tsx
+++ components/shared/InnScreen.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components/native";
+import { Modal, View, Text, Button } from "react-native";
 import { Dimensions } from "react-native";
-import { useContext } from "react";
 import AppContext from "../../helpers/context";
 
 const { width, height } = Dimensions.get('window');
@@ -23,15 +23,51 @@ const CenteredText = styled.Text`
     border-radius: 10px;
 `;
 
+const ModalContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.View`
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    align-items: center;
+    width: 80%;
+`;
+
 const background = require('../../assets/backgrounds/inn.png');
 
 const InnScreen = () => {
     const appContext = useContext(AppContext);
     const player = appContext?.player;
 
+    const [isModalVisible, setModalVisible] = useState(!player?.isBetrayer);
+
+    useEffect(() => {
+        if (!player?.isBetrayer) {
+            setModalVisible(true);
+        }
+    }, []);
+
     return (
         <CustomBackground source={background}>
-            <CenteredText>Inn the Forgotten</CenteredText>
+
+            <Modal
+                transparent
+                visible={isModalVisible}
+                animationType="fade"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <ModalContainer>
+                    <ModalContent>
+                        <Text style={{ fontSize: 18, marginBottom: 20 }}>Hola</Text>
+                        <Button title="Cerrar" onPress={() => setModalVisible(false)} />
+                    </ModalContent>
+                </ModalContainer>
+            </Modal>
         </CustomBackground>
     );
 }
