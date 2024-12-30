f3e31b1e32639996cefc1c26e2c5ede771bb030e
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-30 23:38:13 +0100

    [DEVELOP][UPDATE] Inn background

--- components/shared/InnScreen.tsx
+++ components/shared/InnScreen.tsx
@@ -23,7 +23,7 @@ const CenteredText = styled.Text`
     border-radius: 10px;
 `;
 
-const background = require('../../assets/backgrounds/obituaryBackground.png');
+const background = require('../../assets/backgrounds/inn.png');
 
 const InnScreen = () => {
     const appContext = useContext(AppContext);
