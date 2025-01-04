8b5c1db6cee000c6e504390475619abfe1a58348
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-04 13:36:13 +0100

    [ER6-371][ADD] isArrested attribute for new players

--- src/API/get&post.js
+++ src/API/get&post.js
@@ -153,7 +153,7 @@ export const updateNewAtributtes = async (responseJSON, playerData) => {
     
     const newPlayerData = playerData;
 
-    const newAtributtes = ['isInsideLab', 'isInsideTower', 'fcmToken', 'location', 'isInsideHall', 'isBetrayer, isCaptured'];
+    const newAtributtes = ['isInsideLab', 'isInsideTower', 'fcmToken', 'location', 'isInsideHall', 'isBetrayer, isCaptured, isArrested'];
 
     newAtributtes.forEach(attr => {
         if (attr in responseJSON.data) {
