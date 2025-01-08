56929c617132770de262bb755757e09cf113aaa5
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-08 12:22:50 +0100

    [ER6-391][UPDATE] Get all players fetch

--- App.tsx
+++ App.tsx
@@ -376,7 +376,7 @@ function App(): React.JSX.Element {
       }
 
       setPlayer(player);
-      // player.role = "ACOLYTE";
+      player.role = "ISTVAN";
       await fetchIngredients(player.role);
       setUserRole(player.role);
       await AsyncStorage.setItem("my-role", player.role);
@@ -410,7 +410,7 @@ function App(): React.JSX.Element {
   const getDataAndAsign = async () => {
     const players = await getAllPlayers();
     const data = players.data;
-    setPlayers(data);
+    setPlayers(players);
   }
 
   return (
